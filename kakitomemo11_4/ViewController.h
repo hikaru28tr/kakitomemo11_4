bea7bb00f647c847e974e98eb32fc3239b7668ca
hori hikaru <Hikaru@net75-dhcp90.sfc.keio.ac.jp>
2013-11-04 13:53:17 +0900

    Initial Commit

---
+++ kakitomemo11_4/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  kakitomemo11_4
+//
+//  Created by hori hikaru on 2013/11/04.
+//  Copyright (c) 2013年 hori hikaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ kakitomemo11_4/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  kakitomemo11_4
+//
+//  Created by hori hikaru on 2013/11/04.
+//  Copyright (c) 2013年 hori hikaru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
